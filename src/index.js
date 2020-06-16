9f704bd3b5b6b9f92507e5a569e0ed0bbfda516f
salazenas <salazenas@gmail.com>
2020-06-16 10:30:27 -0300

    persisting cart reducer on storage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.scss";
 import App from "./main/App";
 import * as serviceWorker from "./serviceWorker";
-import store from "./store/index";
+import { store, persistor } from "./store/index";
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore, compose, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import appReducers from "../reducers";
 
 const middlewares = [thunk];
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: "cart"
+};
+
+const persistedReducer = persistReducer(persistConfig, appReducers);
+
 /* eslint-disable no-underscore-dangle */
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
-export default createStore(
-  appReducers,
+const store = createStore(
+  persistedReducer,
   composeEnhancer(applyMiddleware(...middlewares))
 );
+
+const persistor = persistStore(store);
+
+export { store, persistor };
